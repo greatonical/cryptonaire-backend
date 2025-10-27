43327383821d89517ee20ca445d1009f6af6c7ba
Greatonic <awosusigabriel2019@gmail.com>
2025-10-27 09:46:06 +0100

    Fixes Commit

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -6,9 +6,10 @@ import { PrismaService } from '@infra/prisma.service';
 import { JwtStrategy } from '@infra/security/jwt.strategy';
 import { FarcasterController } from './farcaster.controller';
 import { FarcasterService } from './farcaster.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController, MiniKitAuthController, FarcasterController],
-  providers: [SiweService, AuthService, FarcasterService,PrismaService, JwtStrategy]
+  providers: [SiweService, AuthService, FarcasterService,PrismaService, JwtService, JwtStrategy]
 })
 export class AuthModule {}
